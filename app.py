32dbf1cd5db1ff03be09c6c7ffcfbdfabe5e32ed
Genji-MS <Genji.Tapia@students.makeschool.com>
2019-10-02 19:19:38 -0700

    placeholder images, working cards layout

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # app.py
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
-from bson.objectid import ObjectId
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -22,72 +20,6 @@ def index():
     """Return homepage"""
     return render_template('index.html', itemlist = itemlist)
 
-#@app.route('/')
-def playlist_index():
-    """Show store page"""
-    return render_template('playlist_index.html', itemlist= itemlist) #itemlist.find())
-
-@app.route('/playlist/new')
-def playlist_new():
-    """Create New playlist"""
-    return render_template('playlist_new.html', itemlist = {}, title = 'New Playlist')
-
-@app.route('/playlist', methods=['POST'])
-def playlist_submit():
-    """Submit a new playlist"""
-    p_list= { 
-        'title': request.form.get('title'),
-        'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
-    }
-    #Youtube doesn't like to display in this way, replace this component of the URL to embed/ and it'll work fine.
-    for index in range(len(p_list['videos'])):
-        p_list['videos'][index] = p_list['videos'][index].replace("watch?v=", "embed/")
-
-    #playlist_id = itemlist.insert_one(p_list).inserted_id
-    #print(request.form.to_dict())
-    #return redirect(url_for('playlist_index'))
-    #return redirect(url_for('playlist_show', itemlist_id = itemlist_id))
-
-@app.route('/playlist/<playlist_id>')
-def playlist_show(itemlist_id):
-    """Show a single playlist"""
-    #p_list = itemlist.find_one({'_id': ObjectId(itemlist_id)})
-    #p_list_comments = comments.find({'itemlist_id': ObjectId(itemlist_id)})
-    #Double check URL's that may have been saved without /embed/ and check that they even have links.
-    #if p_list.get('videos'):
-    #    for index in range(len(p_list['videos'])):
-    #        p_list['videos'][index] = p_list['videos'][index].replace("watch?v=", "embed/")
-    #return render_template('playlist_show.html',itemlist=p_list,comments=p_list_comments)
-
-@app.route('/playlist/<playlist_id>/edit')
-def playlist_edit(itemlist_id):
-    """Show the edit form for a playlist"""
-    #p_list = itemlist.find_one({'_id': ObjectId(itemlist_id)})
-    #return render_template('playlist_edit.html',itemlist=p_list, title='Edit Playlist')
-
-@app.route('/playlist/<playlist_id>', methods=['POST'])
-def playlist_update(itemlist_id):
-    """Submit and edited playlist"""
-    updated_playlist = {
-        'title': request.form.get('title'),
-        'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
-    }
-    #Youtube doesn't like to display with watch?, replace this component of the URL to embed/ and it'll work fine.
-    for index in range(len(updated_playlist['videos'])):
-        updated_playlist['videos'][index] = updated_playlist['videos'][index].replace("watch?v=", "embed/")
-    #itemlist.update_one(
-        #{'_id':ObjectId(itemlist_id)},
-        #{'$set':updated_playlist})
-    return redirect(url_for('playlist_show', itemlist_id = itemlist_id))
-
-@app.route('/playlist/<playlist_id>/delete', methods=["POST"])
-def playlist_delete(playlist_id):
-    """Delete specified playlist"""
-    #itemlist.delete_one({'_id': ObjectId(itemlist_id)})
-    return redirect(url_for('itemlist_index'))
 
 if app.name == '__main__':
     app.run(debug=True)
-    #app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
